d134b8167a4f066e0c3dc51c4f9fa45bc1f2d0b4
Chris Sevilleja <chris@scotch.io>
2015-03-02 14:59:32 -0800

    reorganizing

---
+++ app1/server.js
@@ -0,0 +1,1 @@
+console.log('Wow! A tiny Node app!');
---
+++ app2/server.js
@@ -0,0 +1,16 @@
+// grab express
+var express = require('express');
+
+// create an express app
+var app = express();
+
+// create an express route for the home page
+// http://localhost:8080/
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+// start the server on port 8080
+app.listen(8080);
+// send a message
+console.log('Server has started!');
